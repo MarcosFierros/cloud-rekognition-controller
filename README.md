2b1eba81d47372334383a1b2f09431740cbfb5de
Marcos Fierros <is702908@iteso.mx>
2021-11-27 12:13:10 -0600

    All the changes in rekognition CONTROLLER

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cloud-rekognition-controller
+
+Cuando se le provee una imágen a este controlador, este utilizará amazon rekognition para poder definir si la imágen se parece a alguna de las guardadas en un bucket de s3
---
+++ index.js
@@ -0,0 +1,116 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express();
+const AWS = require('aws-sdk')
+
+const {
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACESS_KEY, 
+  AWS_SESSION_TOKEN,
+  AWS_REGION_VALUE,
+  S3_BUCKETNAME,
+  REK_FACE_COLLECTION
+} = process.env
+
+if(!AWS_ACCESS_KEY_ID || !AWS_REGION_VALUE || !AWS_SECRET_ACESS_KEY || !AWS_SESSION_TOKEN)
+    throw new Error('Missing aws enviroment vars')
+
+const client = new AWS.Rekognition({
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACESS_KEY,
+  sessionToken: AWS_SESSION_TOKEN,
+  region: AWS_REGION_VALUE
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Api using AWS Rekognition!')
+});
+
+app.listen(8080, async() => {
+  console.log('Example app listening on port 8080!')
+});
+
+// Compare S3 Object to a Rekognition Collection
+app.get('/compare-faces', (req, res) => {
+  const input = {
+    "CollectionId": `${REK_FACE_COLLECTION}`,
+    "FaceMatchThreshold": 95,
+    "Image": {
+      "S3Object": {
+          "Bucket": `${S3_BUCKETNAME}`,
+          "Name": req.body.SourceImage
+      }
+    }
+  }
+  client.searchFacesByImage(input, (err, data) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send(err);
+    }
+    console.log(data);
+    res.status(200).send(data);
+  });
+});
+
+// Create Rekognition Colleciton
+app.post('/create-collection', (req, res) => {
+  const input = {
+    "CollectionId": `${REK_FACE_COLLECTION}`
+  };
+  client.createCollection(input, (err, data) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send(err);
+    }
+    console.log(data);
+    res.status(200).send(data);
+  });
+})
+
+// Add face to Rekognition Collection
+app.post('/index-faces', (req, res) => {
+  const input = {
+    "CollectionId": `${REK_FACE_COLLECTION}`,
+    "MaxFaces": 1,
+    "DetectionAttributes": [],
+    "ExternalImageId": req.body.ExternalID,
+    "Image": {
+      "S3Object": {
+          "Bucket": `${S3_BUCKETNAME}`,
+          "Name": req.body.image
+      }
+    }
+  }
+  client.indexFaces(input, (err, data) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send(error);
+    }
+    console.log(data);
+    res.status(200).send(data);
+  });
+})
+
+// Delete from Rekognition Colleciton
+app.delete('/delete-faces', (req, res) => {
+  const input = {
+    "CollectionId": `${REK_FACE_COLLECTION}`,
+    "FaceIds" : [
+      req.body.FaceIds
+    ]
+  }
+  client.deleteFaces(input, (err, data) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send(err);
+    }
+    console.log(data);
+    res.status(200).send(data);
+  });
+})
+
+
+
